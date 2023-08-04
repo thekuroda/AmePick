a84287f40c3314c4fd0021b8574e19fef1249bce
thekuroda <wakatokuroda@gmail.com>
2023-08-04 15:26:23 +0900

    Add script, Procfile, and requirements
    Added a Python script for data processing, a Procfile for Heroku deployment, and a requirements.txt file for package dependencies

---
+++ diet.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+# Setup ScrapingBee
+api_key = 'IDUZ5WRYJU8NI62W8VMARD1Z3SLYAWYNHFJSWTR9M7CT1GIX052AFKEL1JRNUAQPD7JESV4XJBD4HRGJ'
+url = 'https://blogger.ameba.jp/genres/diet/topics?to=2023-07-26'
+
+response = requests.get(
+    'https://app.scrapingbee.com/api/v1/',
+    params={
+        'api_key': api_key,
+        'url': url,
+    }
+)
+
+# Parse the page source with BeautifulSoup
+soup = BeautifulSoup(response.text, 'html.parser')
+
+# Find all the job posts
+job_posts = soup.find_all('li', class_='p-topics__item')
+
+# Prepare to write to a CSV file
+with open('diet3.csv', 'w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    # Write the header
+    writer.writerow(["URL", "Title", "User"])
+
+    # Write the information of each job post
+    for job_post in job_posts:
+        url = job_post.find('a', class_='p-topics__anchor u-clearfix')['href']
+        title = job_post.find('p', class_='p-topics__title').text
+        user = job_post.find('span', class_='p-topics__userName').text
+        writer.writerow([url, title, user])
