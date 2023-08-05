f1cd1220fd5a299088f9f973e0002f12c7709231
thekuroda <wakatokuroda@gmail.com>
2023-08-05 09:21:53 +0900

    Hide API key
    Using Google Secret Key

--- diet.py
+++ diet.py
@@ -1,9 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+from google.cloud import secretmanager
+
+def access_secret_version(project_id, secret_id, version_id):
+    """
+    Access the payload for the given secret version if one exists. The version
+    can be a version number as a string (e.g. "5") or an alias (e.g. "latest").
+    """
+    client = secretmanager.SecretManagerServiceClient()
+
+    # Build the resource name of the secret version.
+    name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
+
+    # Access the secret version.
+    response = client.access_secret_version(name=name)
+
+    # Return the decoded payload.
+    return response.payload.data.decode('UTF-8')
+
 
 # Setup ScrapingBee
-api_key = 'IDUZ5WRYJU8NI62W8VMARD1Z3SLYAWYNHFJSWTR9M7CT1GIX052AFKEL1JRNUAQPD7JESV4XJBD4HRGJ'
+api_key = access_secret_version("heroic-bird-298712", "scrapingbee", "latest")
 url = 'https://blogger.ameba.jp/genres/diet/topics'
 
 response = requests.get(
